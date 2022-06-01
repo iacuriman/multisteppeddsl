77c574bbe3fab48c24cf4321f4824be73b73d090
iacuriman <95659544+iacuriman@users.noreply.github.com>
2022-06-01 22:55:34 +0200

    Update DeploySpinnaker.groovy

--- job/DeploySpinnaker.groovy
+++ job/DeploySpinnaker.groovy
@@ -20,7 +20,35 @@ def hola = "hola"
                     appstags.put("pasito2", "asc");
                      for (String key : appstags.entrySet()) 
                     {
+ tag = bookMap.get(key)
+                                            activeChoiceParam("${key}") {
+                                                description('Selecione si desea desplegar')
+                                                choiceType('CHECKBOX')
+                                                groovyScript {
+                                                    script("""return ['Deploy']""")
+                                                }
+                                            }
+                                            activeChoiceReactiveParam("ECR_IMAGE_"+"${key}") {
+                                                description('Seleccione la imagen del ECR a desplegar')
+                                                filterable()
+                                                choiceType('SINGLE_SELECT')
+                                                groovyScript {
+                                                    script("""
 
+if("${key}"=="Deploy")
+{return getListEcrImages("eu-west-1","${tag}")}
+""")
+                                                    fallbackScript('return ["error"]')
+                                                }
+                                                referencedParameter("${key}")
+                                            }
+                                            activeChoiceParam("Deploy_ENV_"+"${key}") {
+                                                description('Selecione el entorno a desplegar')
+                                                choiceType('SINGLE_SELECT')
+                                                groovyScript {
+                                                    script("""return ['dev','stg','qa']""")
+                                                }
+                                            }
 
                     }
                                 
